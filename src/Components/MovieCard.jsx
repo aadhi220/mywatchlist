b8e58f326d9fa160bd4e4ff64ea03f04e7d8ccdb
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-20 19:16:12 +0530

    fixed api

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import tmdbAxiosInstance from "../tmdbAxiosInstance";
 
 
-export default function MovieCard({ fetchUrl }) {
+
+export default function MovieCard() {
 
   const [allMovies, setAllMoives] = useState([]);
 
   const baseUrl = "https://image.tmdb.org/t/p/original/";
 
   const fetchData = async () => {
-    const { data } = await tmdbAxiosInstance.get(fetchUrl);
-  
-    setAllMoives(data.results);
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjQwMGRmMDdlZDE1YmFkOGYwNzY4OTljOGUxZjg0YyIsInN1YiI6IjY1MzE3MTc0OGQyMmZjMDE0ZDdjM2UyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jCiXkg1i1zsIquuaVFZRwNR0wQ_rVIyZLl-E9LOahAQ'
+      }
+    };
     
+    fetch('https://api.themoviedb.org/4/account/653171748d22fc014d7c3e22/movie/recommendations?page=1', options)
+      .then(response => response.json())
+      .then(response => {console.log(response) ; 
+        setAllMoives(response.results);
+      
+      }  )
+      .catch(err => console.error(err));
+  
+    
+  
   };
   useEffect(() => {
     fetchData();
@@ -23,13 +37,13 @@ export default function MovieCard({ fetchUrl }) {
     <div className="grid grid-cols-6 gap-4 ">
       
         { allMovies.length>0? allMovies?.map((item,index) => (
-          ( item.name && <div key={index} className="card   w-[220px] p-2 rounded-lg ">
+          ( item.title && <div key={index} className="card   w-[220px] p-2 rounded-lg ">
             <img
               src={`${baseUrl}/${item.poster_path}`}
               alt=""
               className="image w-[200px]"
             />
-            <div className="movie-name">{item.name}</div>
+            <div className="movie-name">{item.title}</div>
             <div>2023</div>
           </div>
         ))): <div>fdsf</div> }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import MovieCard from '../Components/MovieCard'
-import requests from '../requests'
+
 
 export default function Home() {
   return (
-    <div className='p-5  pt-[120px]'><MovieCard fetchUrl={requests.fetchTrending} />
+    <div className='p-5  pt-[120px]'><MovieCard  />
    
     
     

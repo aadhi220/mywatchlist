041e0a87a1808a618177cd08f308ad62d9d2c673
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-21 21:41:42 +0530

    custom json server

---
+++ src/services/allAPI.js
@@ -0,0 +1,73 @@
+import { commonAPI } from "./commonAPI"
+import { serverURL } from "./serverURL"
+
+
+export const AddWatchlist = async (body)=> {
+
+    return await commonAPI("POST",`${serverURL}/watchlist`,body)
+}
+
+
+
+export const getAllWatchlist = async ()=>{
+     
+return await commonAPI("GET",`${serverURL}/watchlist`,"")
+}
+
+
+
+
+export const deleteMovie = async (id)=> {
+
+return await commonAPI("DELETE",`${serverURL}/watchlist/${id}`,{}) 
+}
+
+// export const saveCategory = async (body)=> {
+//        // call post http reqest to http://localhost:4000/categories/ to add categorie to  json server
+// //return response to Category component
+// return await commonAPI("POST",`${serverURL}/categories`,body)  
+// }
+
+// export const getCategory = async ()=> {
+//     // call post http reqest to http://localhost:4000/categories/ to add categorie to  json server
+// //return response to Category component
+// return await commonAPI("GET",`${serverURL}/categories`,"")  
+// }
+// update category from json server
+        // export const updateCategory = async (id,body)=> {
+        //     // call post http reqest to http://localhost:4000/categories/id to update   categories from   json server
+        // //return response to Category component
+        // return await commonAPI("PUT",`${serverURL}/categories/${id}`,body)  
+        // }
+
+
+
+
+
+export const deleteCategory = async (id)=> {
+        // call delete http reqest to http://localhost:4000/categories/id to remove a category from json server
+//return response to Category component
+return await commonAPI("DELETE",`${serverURL}/categories/${id}`,{}) 
+}
+
+export const PostWatchHistory = async (videoHistory)=> {
+           // call post http reqest to http://localhost:4000/videos to store video history to json server
+//return response to videocard component
+return await commonAPI("POST",`${serverURL}/history`,videoHistory)
+}
+
+export const getWatchHistory = async ()=> {
+    // call get http reqest to http://localhost:4000/videos to get history from json server
+//return response to view component
+return await commonAPI("GET",`${serverURL}/history`,"")
+}
+
+// delete history from watch history
+
+export const deleteWatchHistory = async (id)=> {
+    // call delete http reqest to http://localhost:4000/watch history to delete a history from json server
+//return response to watchhistory component
+return await commonAPI("DELETE",`${serverURL}/history/${id}`,{})
+}
+
+
